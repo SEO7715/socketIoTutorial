34d8202a9559ddc32de8380e9c75ae65c3fce7f2
SEO <plant00park21@gmail.com>
2021-11-15 12:38:53 +0900

    room

--- app.js
+++ app.js
@@ -8,23 +8,15 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../socketTutorial/test', 'index.html'));
 });
 
-//create custom namespace basic ver
-// var nsp = io.of('/my-namespace');
-// nsp.on('connection', function(socket) {
-//   console.log('someone connected');
-//   nsp.emit('hi', 'Hello everyone!');
-// });
+// joining Rooms
+var roomno = 1;
+io.on('connection', function(socket) {
+  socket.join("room-" + roomno);
+  // send this event to everyone in the room
+  io.sockets.in("room-" + roomno).emit('connectToRoom', "You are in room no. " + roomno);
 
-const namespace1 = io.of('/namespace1');
-// connection을 받으면, news 이벤트에 description 객체를 담아 보낸다
-namespace1.on('connection', (socket) => {
-  namespace1.emit('news', { description : "someone connected at namespace1"})
-});
+  // socket.leave("room-"+roomno);
 
-
-const namespace2 = io.of('/namespace2');
-namespace2.on('connection', (socket) => {
-  namespace2.emit('news', { description : "someone connected at namespace2"})
 });
 
 http.listen(3000, function() {
